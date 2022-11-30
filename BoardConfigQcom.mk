210674abdc9ed23b0912a4623fa29ef718165bad
Henrique Pereira <hlcpereira@outlook.com.br>
2022-11-30 10:05:30 -0300

    qcom: Split 4.19 SoC family
    * For some reason kona and bengal were splitted within Qualcomm, even though they are from the same "kernel family"
    * With that in mind let's split both HALs set for them
    
    Signed-off-by: Henrique Pereira <hlcpereira@outlook.com.br>
    Change-Id: I09001921515d6a9ffddec725e177ab10ee3a746d

--- BoardConfigQcom.mk
+++ BoardConfigQcom.mk
@@ -17,7 +17,8 @@ UM_PLATFORMS := \
     $(UM_4_4_FAMILY) \
     $(UM_4_9_FAMILY) \
     $(UM_4_14_FAMILY) \
-    $(UM_4_19_FAMILY) \
+    $(UM_4_19_BENGAL_FAMILY) \
+    $(UM_4_19_KONA_FAMILY) \
     $(UM_5_4_FAMILY) \
     $(UM_5_10_FAMILY) \
     $(UM_5_15_FAMILY)
@@ -27,7 +28,8 @@ LEGACY_UM_PLATFORMS := \
     $(UM_4_4_FAMILY) \
     $(UM_4_9_FAMILY) \
     $(UM_4_14_FAMILY) \
-    $(UM_4_19_FAMILY) \
+    $(UM_4_19_BENGAL_FAMILY) \
+    $(UM_4_19_KONA_FAMILY) \
     $(UM_5_4_FAMILY)
 
 QSSI_SUPPORTED_PLATFORMS := \
@@ -35,7 +37,8 @@ QSSI_SUPPORTED_PLATFORMS := \
     $(UM_4_19_LEGACY_FAMILY) \
     $(UM_4_9_FAMILY) \
     $(UM_4_14_FAMILY) \
-    $(UM_4_19_FAMILY) \
+    $(UM_4_19_BENGAL_FAMILY) \
+    $(UM_4_19_KONA_FAMILY) \
     $(UM_5_4_FAMILY) \
     $(UM_5_10_FAMILY) \
     $(UM_5_15_FAMILY)
@@ -96,7 +99,7 @@ TARGET_USES_QCOM_MM_AUDIO := true
 TARGET_USES_COLOR_METADATA := true
 
 # Enable DRM PP driver on UM platforms that support it
-ifneq ($(filter $(UM_4_9_FAMILY) $(UM_4_14_FAMILY) $(UM_4_19_FAMILY) $(UM_5_4_FAMILY) $(UM_5_10_FAMILY) $(UM_5_15_FAMILY),$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter $(UM_4_9_FAMILY) $(UM_4_14_FAMILY) $(UM_4_19_KONA_FAMILY) $(UM_4_19_BENGAL_FAMILY) $(UM_5_4_FAMILY) $(UM_5_10_FAMILY) $(UM_5_15_FAMILY),$(TARGET_BOARD_PLATFORM)),)
     SOONG_CONFIG_qtidisplay_drmpp := true
     TARGET_USES_DRM_PP := true
 endif
@@ -125,12 +128,12 @@ TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS += | (1 << 13)
 TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS += | (1 << 21)
 
 # Mark GRALLOC_USAGE_PRIVATE_HEIF_VIDEO as valid gralloc bit on UM platforms that support it
-ifneq ($(filter $(UM_4_9_FAMILY) $(UM_4_14_FAMILY) $(UM_4_19_FAMILY) $(UM_5_4_FAMILY) $(UM_5_10_FAMILY) $(UM_5_15_FAMILY),$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter $(UM_4_9_FAMILY) $(UM_4_14_FAMILY) $(UM_4_19_KONA_FAMILY) $(UM_4_19_BENGAL_FAMILY) $(UM_5_4_FAMILY) $(UM_5_10_FAMILY) $(UM_5_15_FAMILY),$(TARGET_BOARD_PLATFORM)),)
     TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS += | (1 << 27)
 endif
 
 # List of targets that use master side content protection
-MASTER_SIDE_CP_TARGET_LIST := msm8996 $(UM_4_4_FAMILY) $(UM_4_9_FAMILY) $(UM_4_14_FAMILY) $(UM_4_19_FAMILY)
+MASTER_SIDE_CP_TARGET_LIST := msm8996 $(UM_4_4_FAMILY) $(UM_4_9_FAMILY) $(UM_4_14_FAMILY) $(UM_4_19_KONA_FAMILY) $(UM_4_19_BENGAL_FAMILY)
 
 # Opt-in for old rmnet_data driver
 ifeq ($(filter $(UM_5_15_FAMILY),$(TARGET_BOARD_PLATFORM)),)
@@ -161,9 +164,12 @@ else ifneq ($(filter $(UM_4_9_FAMILY),$(TARGET_BOARD_PLATFORM)),)
 else ifneq ($(filter $(UM_4_14_FAMILY),$(TARGET_BOARD_PLATFORM)),)
     MSM_VIDC_TARGET_LIST := $(UM_4_14_FAMILY)
     QCOM_HARDWARE_VARIANT := sm8150
-else ifneq ($(filter $(UM_4_19_FAMILY),$(TARGET_BOARD_PLATFORM)),)
-    MSM_VIDC_TARGET_LIST := $(UM_4_19_FAMILY)
+else ifneq ($(filter $(UM_4_19_KONA_FAMILY),$(TARGET_BOARD_PLATFORM)),)
+    MSM_VIDC_TARGET_LIST := $(UM_4_19_KONA_FAMILY)
     QCOM_HARDWARE_VARIANT := sm8250
+else ifneq ($(filter $(UM_4_19_BENGAL_FAMILY),$(TARGET_BOARD_PLATFORM)),)
+    MSM_VIDC_TARGET_LIST := $(UM_4_19_BENGAL_FAMILY)
+    QCOM_HARDWARE_VARIANT := bengal
 else ifneq ($(filter $(UM_5_4_FAMILY),$(TARGET_BOARD_PLATFORM)),)
     QCOM_HARDWARE_VARIANT := sm8350
 else ifneq ($(filter $(UM_5_10_FAMILY),$(TARGET_BOARD_PLATFORM)),)
