ca5b51e2c27a0f03f75699adb49f15b295d6358b
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-02-12 07:19:27 -0800

    BoardConfigQcom: Allow use of Specific FM HAL if needed
    Change-Id: I394bee09ff6e763b597ca033f403cb4fb505952c
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- BoardConfigQcom.mk
+++ BoardConfigQcom.mk
@@ -221,3 +221,8 @@ endif
 
 # Add wlan to PRODUCT_SOONG_NAMESPACES
 PRODUCT_SOONG_NAMESPACES += hardware/qcom-caf/wlan
+
+# Add fm-commonsys to PRODUCT_SOONG_NAMESPACES if needed
+ifneq ($(USE_DEVICE_SPECIFIC_FM),true)
+    PRODUCT_SOONG_NAMESPACES += vendor/qcom/opensource/fm-commonsys
+endif
